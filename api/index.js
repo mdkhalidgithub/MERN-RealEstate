71a98126812a54c242dd2ec04665941157412c2c
mdkhalidgithub <mdkhalid8527@gmail.com>
2025-02-20 18:41:41 +0530

    create updaate api route

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import cors from "cors"; // Import cors
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -26,6 +27,8 @@ app.use(cors({
 }));
 
 app.use(express.json());
+
+app.use(cookieParser());
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000!");
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -33,7 +33,7 @@ export default function Profile() {
         className="border p-3 rounded-lg"/>
         <input type="email" placeholder="email" id="email"
         className="border p-3 rounded-lg"/>
-        <input type="text" placeholder="password" id="password"
+        <input type="password" placeholder="password" id="password"
         className="border p-3 rounded-lg"/>
         <button className="bg-slate-700 text-white rounded-lg
         p-3 uppercase hover:opacity-95 disabled:opacity-80">Update</button>
